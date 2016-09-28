c64ca7ca5ecc4976f4c1c795300acfe0c603f673
Telekinesist <Telekinesist@THEPRETTYGOODPC>
2016-09-28 10:13:50 +0200

    Created Interface class

---
+++ MUD/MUD/Interface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUD
+{
+	static class Interface
+	{
+		//Handles user input
+		public static void playerCommand(string input)
+		{
+
+		}
+	}
+}
