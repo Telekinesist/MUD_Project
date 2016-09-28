f5ff2e513698208787cca1a9a2f8ce89c93ad738
Telekinesist <Telekinesist@THEPRETTYGOODPC>
2016-09-28 09:44:53 +0200

    Changed AddEdge back to addEdge because I don't want to rewrite my code.

--- MUD/MUD/Room.cs
+++ MUD/MUD/Room.cs
@@ -21,7 +21,7 @@ namespace MUD
 			id = roomId;
 		}
 
-		public void AddEdge(string direction, Edge linkEdge)
+		public void addEdge(string direction, Edge linkEdge)
 		{
 			switch (direction.Substring(0, 1).ToLower())
 			{
