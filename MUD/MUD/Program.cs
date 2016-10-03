7a24ae3b71a31dae6ed09a213c2fde2da47622ae
Telekinesist <Telekinesist@THEPRETTYGOODPC>
2016-10-03 20:34:25 +0200

    Updated the interface to support mutliple commands for single action in an organized way. Commands are stored as lists in the Data class. Also added up, down, left and right directions simply representing north, south, east and west.

--- MUD/MUD/Program.cs
+++ MUD/MUD/Program.cs
@@ -10,19 +10,22 @@ namespace MUD
 	{
 		static void Main(string[] args)
 		{
+			Data.addCommands();
+
+			//Creates test world
 			Data.world.addRoom(1);
 			Data.world.addRoom(2);
 			
 			Data.getRoom(1).addEdge("north", new Edge(Data.getRoom(2)));
 			Data.getRoom(2).addEdge("south", new Edge(Data.getRoom(1)));
 
-			
 
 			string input = Console.ReadLine().ToLower();
 			while (!(input.Equals("exit")))
 			{
 				Interface.playerCommand(input);
 				input = Console.ReadLine().ToLower();
+				
 			}
 		}
 	}
